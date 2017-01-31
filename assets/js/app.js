21ff4f69c2bd6f6073ad5b7c47d68cb29c7320f8
kkuo57 <kkuo507@gmail.com>
2017-01-31 02:15:37 -0800

    added more items

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,16 @@
 // sanity check
-console.log("app.js linked.");
+//console.log("app.js linked.");
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+}
